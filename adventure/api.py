9e398b6d7f99be50097ebcb8a6d54093cc303203
arturolei <artie.lei@gmail.com>
2020-03-02 20:27:54 -0800

    Trying another fix; need to set parameter to true or false

--- adventure/api.py
+++ adventure/api.py
@@ -16,7 +16,7 @@ def rooms(request):
     room_list = []
     for room in Room.objects.all():
         room_list.append({"id":room.id, "title":room.title, "description": room.description, "n_to":room.n_to, "s_to":room.s_to, "e_to":room.e_to, "w_to":room.w_to})
-    return JsonResponse(room_list)
+    return JsonResponse(room_list, safe = True)
 
 
 
