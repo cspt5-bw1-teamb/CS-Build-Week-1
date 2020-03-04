f95d9f2d3c51e8af49a4c3b85787d0760c74875a
arturolei <artie.lei@gmail.com>
2020-03-03 21:42:11 -0800

    Fixed rooms api endpoint output

--- adventure/api.py
+++ adventure/api.py
@@ -14,7 +14,7 @@ import json
 @api_view(["GET"])
 def rooms(request):
     room_list = []
-    for room in Room.objects.all():
+    for room in Room.objects.all().order_by('id'):
         room_list.append({"id":room.id, "title":room.title, "description": room.description, "n_to":room.n_to, "s_to":room.s_to, "e_to":room.e_to, "w_to":room.w_to, "players":room.playerNames(request.user.player.id)})
     return JsonResponse(room_list, safe = False)
 
